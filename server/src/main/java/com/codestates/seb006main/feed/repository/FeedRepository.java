b71e3b820ccbc9761e83fa04fecf2c2c68f6281c
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2022-10-24 17:50:02 +0900

    delete

--- server/src/main/java/com/codestates/seb006main/feed/repository/FeedRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.seb006main.feed.repository;
-
-import com.codestates.seb006main.feed.entity.Feed;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FeedRepository extends JpaRepository<Feed, Long> {
-}
--- server/src/main/java/com/codestates/seb006main/matching/repository/MatchingRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codestates.seb006main.matching.repository;
-
-import com.codestates.seb006main.matching.entity.Matching;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MatchingRepository extends JpaRepository<Matching, Long>, MatchingRepositoryCustom {
-}
